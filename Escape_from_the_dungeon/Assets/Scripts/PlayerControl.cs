b7ffc263849d00535a5a9d07e8874d81b6e5927a
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2021-10-12 16:15:28 -0700

    Created PointToMouse.cs
    PointsToMouse.cs rotates Transform on an object based on the position of the mouse

--- Escape_from_the_dungeon/Assets/Scripts/PlayerControl.cs
+++ Escape_from_the_dungeon/Assets/Scripts/PlayerControl.cs
@@ -5,24 +5,46 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
-    [SerializeField] public GameObject player;
+    // GameObjects
+    private GameObject player;
+    private GameObject gun;
+    private GameObject jetpack;
+
+    // Characteristics
+    public int speed;
 
     // Start is called before the first frame update
     void Start()
     {
         // player.GameObject.Find("Player");
-        Transform playerTransform = player.GetComponent<Transform>();
+        //Transform playerTransform = player.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("d"))
+        if(Input.GetKey("d"))
         {
             // playerTransform.Translate(10, 0, 0); // .x += 10;
             // player.Translate(10,0,0);
-            player.transform.position = player.transform.position + new Vector3(10, 0, 0);
-            
+            // player.transform.position = player.transform.position + new Vector3(3, 0, 0);
+            //transform.Translate(speed*Time.deltaTime, 0f, 0f);
+            Debug.Log("Moved right.");
+        } 
+        else if (Input.GetKey("a"))
+        {
+            // player.transform.position = player.transform.position + new Vector3(-3, 0, 0);
+            //transform.Translate(-speed*Time.deltaTime, 0f, 0f);
+            Debug.Log("Moved left.");
+        } 
+        else  if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Pressed left click.");
+
+        } 
+        else if (Input.GetMouseButton(1))
+        {
+            Debug.Log("Pressed right click.");
         }
     }
 }
