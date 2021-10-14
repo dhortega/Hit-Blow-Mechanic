3e2866fefdbad4984541e45272cc152dd052ddf6
JaouShingan <lky13587089578@gmail.com>
2021-10-14 12:13:43 -0700

    Version1.35
    Change the air instead of water

--- Escape_from_the_dungeon/Assets/Scripts/PlayerControl.cs
+++ Escape_from_the_dungeon/Assets/Scripts/PlayerControl.cs
@@ -15,8 +15,8 @@ public class PlayerControl : MonoBehaviour
     // Characteristics
     public int speed;
     public int flyspeed;
-    public double waterbar;
-    public double waterspeed;
+    public double airbar;
+    public double airspeed;
     public GameObject bulletprefab;
     public GameObject bullethole;
     public float bulletspeed;
@@ -44,8 +44,8 @@ public class PlayerControl : MonoBehaviour
             // Translate speed of the object attached to
             transform.Translate(speed*Time.deltaTime, 0f, 0f);
             Debug.Log("Moved right.");
-            if (waterbar < 100)
-                waterbar += Time.deltaTime * (waterspeed - 50);
+            if (airbar < 100)
+                airbar += Time.deltaTime * (airspeed - 50);
         } 
         else if (Input.GetKey("a"))
         {
@@ -55,21 +55,21 @@ public class PlayerControl : MonoBehaviour
             // Translate speed of the object attached to
             transform.Translate(-speed*Time.deltaTime, 0f, 0f);
             Debug.Log("Moved left.");
-            if (waterbar < 100)
-                waterbar += Time.deltaTime * (waterspeed - 50);
+            if (airbar < 100)
+                airbar += Time.deltaTime * (airspeed - 50);
         } 
         else  if (Input.GetMouseButton(0))
         {
-            if (waterbar>0)
+            if (airbar>0)
             {
                 Debug.Log("Pressed left click.");
                 transform.Translate(0f, flyspeed * Time.deltaTime, 0f);
-                waterbar -= Time.deltaTime * waterspeed;
+                airbar -= Time.deltaTime * airspeed;
                 Debug.Log("waterbar");
             }
             else
             {
-                Debug.Log("There is no water in the waterbag");
+                Debug.Log("There is no air in the airbag");
             }
 
         }
@@ -77,8 +77,8 @@ public class PlayerControl : MonoBehaviour
         else if (Input.GetMouseButtonDown(1))
         {
 
-            if (waterbar < 100)
-                waterbar += Time.deltaTime * (waterspeed - 50);
+            if (airbar < 100)
+                airbar += Time.deltaTime * (airspeed - 50);
             Debug.Log("Pressed right click.");
             
             // Jetpack RidigBody
@@ -92,8 +92,8 @@ public class PlayerControl : MonoBehaviour
             Debug.Log(bullethole.transform.position);
             bullet.transform.Translate((jetpack.transform.position-bullethole.transform.position)*bulletspeed);
         }
-        if (waterbar < 100)
-            waterbar += Time.deltaTime * (waterspeed - 50);
+        if (airbar < 100)
+            airbar += Time.deltaTime * (airspeed - 50);
 
     }
 }
