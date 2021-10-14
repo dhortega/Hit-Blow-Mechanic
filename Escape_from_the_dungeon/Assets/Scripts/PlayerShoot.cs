017d3dcd6d56437d6287d17b31dcd6ed1ed3b71f
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2021-10-14 14:58:21 -0700

    Created PlayerShoot.cs
    Took code from PlayerControl and moved it into another script called PlayerShoot.cs.
    PlayerShoot.cs creates bullets at the transform that the script is attached to.

---
+++ Escape_from_the_dungeon/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+    // Spawns a bullet with a particular bullet at the transform where this script is attached
+    public void SpawnProjectile(GameObject bulletprefab, float bulletspeed)
+    {
+        Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x,Input.mousePosition.y));
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y); //+ 1
+        Vector2 direction = target - myPos;
+        direction.Normalize();
+        GameObject projectile = (GameObject)Instantiate(bulletprefab, myPos, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().velocity = direction * bulletspeed;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
