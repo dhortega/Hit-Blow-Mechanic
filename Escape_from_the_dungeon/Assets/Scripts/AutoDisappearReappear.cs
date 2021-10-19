5137af6c45f992b81e2d9429df9046e69be79f3c
Daniel Jarreau Ortega <dhortega@ucsc.edu>
2021-10-19 02:23:42 -0700

    Updated AutoDisappearReappear.cs
    Attempted to add the AutoDisappearReappear.cs behavior to Rock.prefab and disabling and reenabling RockSpawner

--- Escape_from_the_dungeon/Assets/Scripts/AutoDisappearReappear.cs
+++ Escape_from_the_dungeon/Assets/Scripts/AutoDisappearReappear.cs
@@ -6,6 +6,7 @@ public class AutoDisappearReappear : MonoBehaviour
 {
     // Outside script
     PlatformManager platManager;
+    GameObject rockSpawner;
     //public Renderer platRenderer;
     //public Rigidbody2D platRigidbody2D;
     //public Collider2D platCollider2D;
@@ -14,6 +15,7 @@ public class AutoDisappearReappear : MonoBehaviour
     void Start()
     {
         platManager = GameObject.Find("PlatformManager").GetComponent<PlatformManager>();
+        rockSpawner = GameObject.Find("RockSpawner");
         //platRenderer = GetComponent<Renderer>();
         //platRigidbody2D = GetComponent<Rigidbody2D>();
         //platCollider2D = GetComponent<Collider2D>();
@@ -27,7 +29,7 @@ public class AutoDisappearReappear : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // If this game object collides with the game object tagged Player
-        if (collision.collider.tag == "Player")
+        if (collision.collider.tag == "Player" && gameObject.tag == "Ground")
         {
             Debug.Log(message: $"<color=blue><size=16>Entered blue platform collision</size></color>");
             // Delays the disappearing of the gameObject being collided with
@@ -35,7 +37,22 @@ public class AutoDisappearReappear : MonoBehaviour
             platManager.delay(platManager.platformsArray, this.gameObject);
             //gameObject.SetActive(true);
         }
+        else if (collision.collider.tag == "Bullet") // Refactor this code. This currently doesn't work
+        {
+            Invoke("deactivateGameObject", 2f); // Cannot use invoke if the function being called has parameters
+                                               
+            Invoke("reactivateGameObject", 5f);
+        }
     }
 
-    
+    public void deactivateGameObject()
+    {
+        rockSpawner.SetActive(false); // Disables the searched game object
+    }
+
+    public void reactivateGameObject()
+    {
+        rockSpawner.SetActive(true);
+    }
+    //yield return new WaitForSeconds(0);
 }
